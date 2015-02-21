1a9144fce2176c93daa4f82cbf459763e41c3977
Jim89 <jimmy22theave@googlemail.com>
2015-02-21 10:42:31 +0000

    Updates to code to build final model, perform cross validation and output test cases for submission

--- projectCode.R
+++ projectCode.R
@@ -1,6 +1,8 @@
 ################################################################################
 # set up
 ################################################################################
+#dir <- "/media/storage/Documents/R/practicalMachineLearning/project"
+
 # clear out environment
   rm(list = ls())
 
@@ -9,7 +11,7 @@
   library(ggplot2, quietly = T)
   library(caret, quietly = T)
   library(randomForest, quietly = T)
-
+  library(rattle, quietly = T)
 
 ################################################################################
 # get the data
@@ -24,11 +26,11 @@
   }
 # train
   if(!file.exists("./data/train.csv")){
-    download.file(trainURL, "./data/train.csv")
+    download.file(trainURL, "./data/train.csv", method = "curl")
   }
 # test
   if(!file.exists("./data/test.csv")){
-    download.file(testURL, "./data/test.csv")
+    download.file(testURL, "./data/test.csv", method = "curl")
   }
 
   rm(testURL)
@@ -46,37 +48,50 @@
 
 # remove the junk fields
   data <- data[,-c(1:7)]
-  data <- data[,colSums(is.na(data)) < (0.3*nrow(data))]
+  data <- data[,colSums(is.na(data)) < 0.3*nrow(data)]
 
 # check the count of missing values per field
   missing <- sapply(1:(ncol(data)-1), function(x) data[,x] %>% is.na() %>% sum())
-
+# none! 
 
 # check what sort of data we're working with - expecting mainly numeric
-  classes <- sapply(1:(ncol(data)-1), function(x) data[,x] %>% class()) # lots of factors
+  classes <- sapply(1:(ncol(data)-1), function(x) data[,x] %>% class()) # all numeric/int
 
 
 ################################################################################
 # fit a model
 ################################################################################
-index <- createDataPartition(data$classe, p = 0.8, list = F)
-train <- data[index,]
-test <- data[-index,]
-rf <- randomForest(classe ~ ., data = train, ntree = 100); rf
+# partition the data into training and test
+  index <- createDataPartition(data$classe, p = 0.8, list = F)
+  train <- data[index,]
+  test <- data[-index,]
 
-names(rf)
+# fit the model - try random forest first as the data are not large and it's a robust method
+ptm <- proc.time()
+rf <- randomForest(classe ~ ., data = train, ntree = 201, importance = T)
+proc.time() - ptm
 
 testOutcome <- predict(rf, test)
-confusionMatrix(testOutcome, test$classe)
+confMatrix <- confusionMatrix(testOutcome, test$classe)
 
+# looks like the model is highly accurate, even on the held out test set. Apply to real validation set
 
+################################################################################
+# apply to validation set
+################################################################################
 
-pml_write_files = function(x){
+validation <- read.csv("./data/test.csv", header = T, na.strings = c("#DIV/0!", "NA"))
+
+predictions <- predict(rf, validation) %>% as.character()
+
+if(!file.exists("./outputs")){dir.create("./outputs")}
+
+pml_write_files <- function(x){
   n = length(x)
   for(i in 1:n){
-    filename = paste0("problem_id_",i,".txt")
+    filename = paste0("./outputs/problem_id_",i,".txt")
     write.table(x[i],file=filename,quote=FALSE,row.names=FALSE,col.names=FALSE)
   }
 }
 
-
+pml_write_files(predictions)
